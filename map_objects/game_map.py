1cd661e39bea679c05a7d3f6c4c656eef373b69e
Victor Dominoni <dominoni.victor@gmail.com>
2019-05-05 22:39:02 -0300

    Parte 2 com solução desafio 2.A

--- map_objects/game_map.py
+++ map_objects/game_map.py
@@ -16,20 +16,21 @@ class GameMap:
         tiles[31][22].block_sight = True
         tiles[32][22].blocked = True
         tiles[32][22].block_sight = True
-        
+      
         for x in range(30, 36):
             tiles[x][x].blocked = True
             tiles[x][x].block_sight = True
+            
         for x in range(35, 41):
             tiles[x][(70-x)].blocked = True
             tiles[x][(70-x)].block_sight = True    
             
-        tiles[30+5][22].blocked = True
-        tiles[30+5][22].block_sight = True
-        tiles[31+5][22].blocked = True
-        tiles[31+5][22].block_sight = True
-        tiles[32+5][22].blocked = True
-        tiles[32+5][22].block_sight = True
+        tiles[30+7][22].blocked = True
+        tiles[30+7][22].block_sight = True
+        tiles[31+7][22].blocked = True
+        tiles[31+7][22].block_sight = True
+        tiles[32+7][22].blocked = True
+        tiles[32+7][22].block_sight = True
         
         return tiles
         
